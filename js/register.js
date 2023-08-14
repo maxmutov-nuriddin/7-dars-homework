3a44dc1bd3d134f8fc1b931ac51c8675d36f497a
maxmutov-nuriddin <mahmudovnuriddin35@gmail.com>
2023-08-15 02:41:26 +0500

    add loader

--- js/register.js
+++ js/register.js
@@ -1,20 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let email = document.querySelector('.email');
-  let password = document.querySelector('.password');
-  let btn = document.querySelector('.btn');
-
-  btn.addEventListener('click', function (event) {
-    event.preventDefault();
-
-    let userEmail = email.value;
-    let userPassword = password.value;
-
-    if (!userEmail || !userPassword) {
-      alert('Пожалуйста, заполните все поля.');
-    } else if (!userEmail.includes('@')) {
-      alert('Пожалуйста, введите корректный email адрес.');
-    } else {
+  if (!localStorage.getItem('firstVisit')) {
+    localStorage.setItem('firstVisit', 'true');
+    box.classList.add('hidden')
+    showLoader();
+    setTimeout(function () {
       window.location.href = '../data.html';
-    }
-  });
+    }, 2000);
+  } else {
+    let email = document.querySelector('.email');
+    let password = document.querySelector('.password');
+    let btn = document.querySelector('.btn');
+    let box = document.querySelector('.container');
+    let body = document.querySelector('.body');
+
+    btn.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      let userEmail = email.value;
+      let userPassword = password.value;
+
+      if (!userEmail || !userPassword) {
+        alert('Пожалуйста, заполните все поля.');
+      } else if (!userEmail.includes('@')) {
+        alert('Пожалуйста, введите корректный email адрес.');
+      } else {
+        box.classList.add('hidden');
+        showLoader();
+        setTimeout(function () {
+          window.location.href = '../data.html';
+        }, 2000);
+      }
+    });
+  }
 });
+
+function showLoader() {
+  let loader = document.createElement('div');
+  loader.className = 'spinner';
+  for (let i = 0; i < 3; i++) {
+    let div = document.createElement('div');
+    loader.prepend(div)
+  }
+  document.body.appendChild(loader);
+}
